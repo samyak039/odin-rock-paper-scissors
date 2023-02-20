aa48bd45167cd11a8e7af5c82bb7ba35f30afe6d
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-20 13:56:38 +0530

    Implement `play()`

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let choices = ["Rock", "Paper", "Scissors"];
+let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
+}
+
+function play(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (playerChoice === computerChoice) {
+    return `It's Draw! ${playerChoice} and ${computerChoice}`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return `You Won! ${playerChoice} beats ${computerChoice}`;
+  } else {
+    return `You Lose! ${computerChoice} beats ${playerChoice}`;
+  }
 }
